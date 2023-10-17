1f2970d1a40a06ab6825413bd90e3ec03f223d94
Thomas Seilund <tps@netmaster.dk>
2023-10-17 14:24:32 +0200

    Initial Commit

---
+++ ICB.app.iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ICB.app.iOS
+//
+//  Created by Thomas Seilund on 17/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ICB.app.iOS/ICB_app_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ICB_app_iOSApp.swift
+//  ICB.app.iOS
+//
+//  Created by Thomas Seilund on 17/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ICB_app_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
